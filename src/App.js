34f3130d0212199604be0f4f24bbe06e361f8699
Mark Krensel <mark.krensel@gmail.com>
2022-09-29 16:30:19 +0200

    add simple html markup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="React and Morty"></header>
+      <header className="Header">React and Morty</header>
+      <ul>
+        <li>
+          <p>Morty Smith</p>
+          <img
+            src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+            alt=""
+          />
+        </li>
+      </ul>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Favourite</li>
+          <li>Special1</li>
+          <li>Special2</li>
+        </ul>
+      </nav>
     </div>
   );
 }
